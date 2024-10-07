8f2389f1512ea756024283a2142c010804407827
samueljsong <semuall0108@gmail.com>
2024-10-07 13:12:08 -0700

    Started learning C#

---
+++ WriteYourFirstC#Code.cs
@@ -0,0 +1,14 @@
+/*
+ *  Difference between Console.Write() and Console.WriteLine()
+ *  Console.Write();        --> Does not make a new line
+ *  Console.WriteLine();    --> Appends a new line after the output   
+ * 
+ *  The Compiler is used for converting code into something the computer can understand. 
+*/
+
+Console.WriteLine("Hello");
+Console.WriteLine("Word");
+
+Console.Write("This")
+Console.Write("Will");
+Console.Write("Make a new line");
